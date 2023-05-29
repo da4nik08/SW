53131ca94a867cdbd1cf01afc4dca3c4979b8f8d
da4nik08 <illia.cherniavskyi1@nure.ua>
2023-05-29 14:22:05 +0300

    28.05

--- collate.py
+++ collate.py
@@ -1,11 +1,14 @@
 import torch
 from scipy import signal
 import numpy as np
+import librosa
+import sklearn
+from sklearn import preprocessing
 
 def process_data(np_wav, nperseg=1024, samplerate=24000):
     # Function to process a single piece of data
     # Implement your custom processing logic here
-    _, _, processed_data = signal.stft(np_wav, samplerate, nperseg=nperseg)  # Example processing: doubling the data
+    _, _, processed_data = signal.stft(np_wav, samplerate, nperseg=nperseg, return_onesided=True)
     pd = np.power(np.abs(processed_data), 0.5)
     mean = np.mean(pd, axis=1, keepdims=True)
     std = np.std(pd, axis=1, keepdims=True)
@@ -13,11 +16,21 @@ def process_data(np_wav, nperseg=1024, samplerate=24000):
     new_shape = int(939 * 1024/nperseg)
     return torch.tensor(np.pad(pd, ((0, 0), (0, new_shape - pd.shape[1])), mode='constant'))
 
+def process_data_mfcc(np_wav, nperseg=512, samplerate=24000):
+    mfcc = librosa.feature.mfcc(y=np_wav.astype(float), sr=samplerate, hop_length=nperseg)
+    pd = sklearn.preprocessing.scale(mfcc, axis=1)
+    mean = np.mean(pd, axis=1, keepdims=True)
+    std = np.std(pd, axis=1, keepdims=True)
+    pd = (pd - mean) / std
+    new_shape = int(469 * 1024/nperseg)
+    return torch.tensor(np.pad(pd, ((0, 0), (0, new_shape - pd.shape[1])), mode='constant'))
+    
+
 def collate_fn(batch):
     features_batch, labels_batch = zip(*batch)
     
     processed_batch = []
     for data in features_batch:
-        processed_data = process_data(data)
+        processed_data = process_data_mfcc(data)
         processed_batch.append(processed_data)
-    return torch.stack(processed_batch), torch.stack(labels_batch)
+    return torch.stack(processed_batch)#, torch.tensor(labels_batch)
