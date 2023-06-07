325f4f97ea0b3b1dcb2f33a3e9d22c9f0d310fd6
da4nik08 <illia.cherniavskyi1@nure.ua>
2023-06-07 23:39:24 +0300

    07.06

--- collate.py
+++ collate.py
@@ -6,7 +6,7 @@ import sklearn
 from sklearn import preprocessing
 from custom_dataset import CustomDataset
 
-def process_data(np_wav, nperseg=1024, samplerate=24000):
+def process_data(np_wav, nperseg=1024, samplerate=24000): # (513, n) (127[2],234[2]) reshape=> (234, 127)
     # Function to process a single piece of data
     # Implement your custom processing logic here
     _, _, processed_data = signal.stft(np_wav, samplerate, nperseg=nperseg, return_onesided=True)
@@ -16,14 +16,14 @@ def process_data(np_wav, nperseg=1024, samplerate=24000):
     pd = (pd - mean) / std
     new_shape = int(939 * 1024/nperseg)
     pad = np.pad(pd, ((0, 0), (0, new_shape - pd.shape[1])), mode='constant')
-    return torch.tensor(np.expand_dims(np.swapaxes(pad,0,1), axis=0))
+    return torch.tensor(np.expand_dims(np.swapaxes(pad,0,1), axis=0), dtype=torch.float)
 
 def process_data_mfcc(np_wav, nperseg=1024, samplerate=24000):
     mfcc = librosa.feature.mfcc(y=np_wav.astype(float), sr=samplerate, hop_length=nperseg)
     pd = sklearn.preprocessing.scale(mfcc, axis=1)
-    mean = np.mean(pd, axis=1, keepdims=True)
-    std = np.std(pd, axis=1, keepdims=True)
-    pd = (pd - mean) / std
+    #mean = np.mean(pd, axis=1, keepdims=True)
+    #std = np.std(pd, axis=1, keepdims=True)
+    #pd = (pd - mean) / std
     new_shape = int(469 * 1024/nperseg)
     pad = np.pad(pd, ((0, 0), (0, new_shape - pd.shape[1])), mode='constant')
     return torch.tensor(np.expand_dims(np.swapaxes(pad,0,1), axis=0), dtype=torch.float)
@@ -34,6 +34,7 @@ def collate_fn(batch):
     
     processed_batch = []
     for data in features_batch:
-        processed_data = process_data_mfcc(data)
+        #processed_data = process_data_mfcc(data)
+        processed_data = torch.tensor(data, dtype=torch.float)
         processed_batch.append(processed_data)
     return torch.stack(processed_batch), torch.tensor(labels_batch)
